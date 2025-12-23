f7b53cc287029d32e66444f70f6be2eb587516ff
sleepercode21-glitch <sleepercode21@gmail.com>
2025-12-23 00:05:32 -0700

    verification added

--- src/add/Add.java
+++ src/add/Add.java
@@ -1,6 +1,5 @@
 package add;
 
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -9,7 +8,9 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Formatter;
 import java.nio.charset.StandardCharsets;
+import java.util.zip.Inflater;
 import java.util.zip.Deflater;
+import java.util.zip.DataFormatException;
 import java.io.ByteArrayOutputStream;
 import java.util.Arrays;
 import java.nio.ByteBuffer;
@@ -21,17 +22,42 @@ public class Add {
     private static byte[] compress (byte[] bytes) {
         Deflater deflater = new Deflater(Deflater.DEFAULT_COMPRESSION);
         deflater.setInput(bytes);
+        // indicates no more input data
         deflater.finish();
-
+        
         ByteArrayOutputStream output = new ByteArrayOutputStream(bytes.length);
+        // allot buffer size 8kb 
         byte[] buffer = new byte[8192];
 
         while (!deflater.finished()) {
+            // count - how many bytes were read into the buffer
             int count = deflater.deflate(buffer);
+            // only write buffer from index 0 to count
             output.write(buffer, 0, count);
         }
         
         deflater.end();
+        return output.toByteArray();
+    }
+
+    public static byte[] decompress(byte[] compressed) {
+        Inflater inflater = new Inflater();
+        inflater.setInput(compressed);
+        
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        byte[] buffer = new byte[8192];
+        
+        try {
+            while (!inflater.finished()) {
+                int count = inflater.inflate(buffer);
+                output.write(buffer, 0, count);
+            }
+        } catch (DataFormatException e){
+            throw new RuntimeException("Currupt compressed data", e);
+        } finally {
+            inflater.end();
+        }
+
         return output.toByteArray();
     }
 
@@ -56,34 +82,62 @@ public class Add {
         return result;
     }
 
+    private static boolean verify(String fileDirectory) {
+       try (FileInputStream inputStream = new FileInputStream(fileDirectory)) {
+          byte[] compressed = inputStream.readAllBytes();
+          byte[] decompressed = decompress(compressed); 
+          String hash = SHAsum(decompressed);
+          System.out.println("hash generated: " + hash);
+          String expectedHash = fileDirectory.substring(fileDirectory.lastIndexOf("/")
+                                                        - 2).replace("/", ""); 
+          return expectedHash.equals(hash);
+        
+       } catch (IOException e) {
+          System.err.println("Failed to verify file: " + fileDirectory);
+          e.printStackTrace();
+       }
+        
+        return false;
+    }
     public void execute(String[] files) {
         for (String filePath : files) {
             try (FileInputStream inputStream = new FileInputStream(filePath)) {
                 
-                int size = inputStream.available();
-                String stringHeader = "blob " + size + "\0"; 
-                byte[] byteHeader = stringHeader.getBytes(StandardCharsets.UTF_8); 
                 byte[] byteContent = inputStream.readAllBytes();
+                int size = byteContent.length;
+                String stringHeader = "blob " + size + "\0"; 
+                byte[] byteHeader = stringHeader.getBytes(StandardCharsets.US_ASCII); 
                 byte[] byteFile = ByteBuffer.allocate(byteHeader.length + 
                                                       byteContent.length) 
                                                       .put(byteHeader)
                                                       .put(byteContent)
                                                       .array(); 
                 String hash = SHAsum(byteFile);
+                System.out.println("file hash: " + hash);
                 byte[] compressed = compress(byteFile);
                 String directory = ".jit/objects/" + hash.substring(0, 2);
-                String fileDirectory = ".jit/objects/" + hash.substring(0, 2) + "/"
-                                                     + "/" + hash.substring(2);
+                String fileDirectory = ".jit/objects/" + hash.substring(0, 2) + 
+                                                     "/" + hash.substring(2);
                 // making directory to save file 
                 File objectDirectory = new File(directory);
                 objectDirectory.mkdirs();
-                File file = new File(fileDirectory);
                 
+                // making a file directory
+                File file = new File(fileDirectory); 
+
                 if (!file.exists()) {
                     try (FileOutputStream out = new FileOutputStream(fileDirectory)) {
                         out.write(compressed);
+                        if (verify(fileDirectory)) {
+                            System.out.println("File verified!");
+                        } else {
+                            System.out.println("Could not verify file " + 
+                            fileDirectory);
+                        }
                     }
-                }         
+                }
+
+                
             } catch (IOException e) {
                 System.err.println("Failed to read file: " + filePath);
                 e.printStackTrace();
